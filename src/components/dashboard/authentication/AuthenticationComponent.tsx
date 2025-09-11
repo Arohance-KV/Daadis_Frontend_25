5e529bd6b3e40eccf47ce14f97dd815ef612599b
Rohan Sunwar <rohansunwar5@gmail.com>
2025-09-11 14:25:30 +0530

    updated google auth

--- src/components/dashboard/authentication/AuthenticationComponent.tsx
+++ src/components/dashboard/authentication/AuthenticationComponent.tsx
@@ -27,40 +27,6 @@ export const loginFormSchema = z.object({
     password: z.string().min(8, { message: "Password must be 8 characters long"}).refine((password) => /[a-z]/.test(password), { message: "Password must contain at least one lower case character!"}).refine((password) => /[A-Z]/.test(password), { message: "Password must contain at least one upper case character!"}).refine((password) => /[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/.test(password), { message: "Password must contain at least one special character!"}).refine((password) => /[0-9]/.test(password), { message: "Password must contain at least one numberic value!"})
 });
 
-// const textFieldTheme = createTheme({
-//     components: {
-//         MuiTextField: {
-//             styleOverrides: {
-//                 root: {
-//                     "& .MuiInputBase-root" : {
-//                         color: `${yellow[600]}`,
-//                     },
-//                     "& .MuiInputLabel-root" : {
-//                         // color: `${yellow[300]}`
-//                     },
-//                     "& .MuiInputLabel-root.Mui-focused" : {
-//                         color: `${yellow[600]}`
-//                     },
-//                     "& .MuiOutlinedInput-root" : {
-//                         "& filedset" : {
-//                             borderColor: `${"purple"}`
-//                         },
-//                         '&:hover fieldset': {
-//                             borderColor: `${yellow[500]}`,
-//                           },
-//                         '&.Mui-focused fieldset': {
-//                             borderColor: `${yellow[600]}`,
-//                         },
-//                         '& .MuiOutlinedInput-input': {
-//                             color: `${yellow[600]}`, 
-//                         },
-//                     },
-//                 },
-//             }
-//         }
-//     }
-// })
-
 export const mergeCart = (guestCart : ICartItem[], mainCart : ICartItem[]) => {
         
     console.log(`guestCart`, guestCart, `mainCart`, mainCart);
@@ -95,8 +61,6 @@ export const mergeCart = (guestCart : ICartItem[], mainCart : ICartItem[]) => {
 };
 
 export const AuthenticationComponent = ({ type } : { type: string }) => {
-    
-    // const userData = useSelector((state: any) => state.userData);
 
     
     const dispatch = useDispatch();
@@ -141,66 +105,7 @@ export const AuthenticationComponent = ({ type } : { type: string }) => {
         })();        
     }, []);
 
-    // const loginForm = useForm<z.infer<typeof loginFormSchema>>({
-    //     resolver: zodResolver(loginFormSchema),
-    //     defaultValues: {
-    //         email: "",
-    //         password: ""
-    //     }
-    // });
-
     
-
-    // const [ isLoginButtonLoading, setIsLoginButtonLoading ] = useState(false);
-
-    // const onLoginFormSubmit = async (values: z.infer<typeof loginFormSchema>) => {
-    //     console.log(values);
-    //     setIsLoginButtonLoading(true);
-    //     try {
-    //         // @ts-ignore
-    //         const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}${import.meta.env.VITE_PORT}${import.meta.env.VITE_API_URL}users/login`, {
-    //             method: "POST",
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //             },
-    //             credentials: 'include',
-    //             body: JSON.stringify({email: values.email, password: values.password})
-    //         });
-
-    //         if (!response.ok) {
-    //             const errData = await response.json();
-    //             loginForm.setError(errData.errors[0].type, {type: "manual", message: errData.errors[0].errMsg})
-    //             throw new Error("HTTP error! status: "+response.status);
-    //         }
-
-    //         console.log(response);
-            
-    //         const data = await response.json();
-
-    //         if ( data.data.user.role !== "Admin" ) {
-    //             loginForm.setError("email", { type: "manual", message: "Unauthorized user!"})
-    //             throw new Error("Unauthorized user");
-    //         }
-            
-    //         dispatch(setUser(data.data.user));
-    //         navigate("/admin/dashboard/home");
-            
-    //         console.log(data);
-    //     } catch (error) {
-    //         console.error("Error: ", error);
-    //     }
-    //     setIsLoginButtonLoading(false);
-    // };
-
-    // const [ showPassword, setShowPassword ] = useState(false);
-
-
-    // const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-
-    // const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-    //   event.preventDefault();
-    // };
 
     return (
         <div className={cn(`w-full font-[quicksand] flex justify-center items-center`, (type === "dashboard") && "h-screen")}>
@@ -256,44 +161,7 @@ export const AuthenticationComponent = ({ type } : { type: string }) => {
                         
                         const updateCartData = await updateCartResponse.json();
 
-                        // if (data.data.role !== "Customer") throw new Error(`Error: ${401}, Unauthorised user`);
-                        // dispatch(setCustomerData(updateCartData.data));
                         console.log(updateCartData);
-                        // return true;
-                        // guestWishList.forEach((item : IWishListItem) => {
-                        //     if ( guestWishList?.filter((wishListItem : IWishListItem) => wishListItem?.product?._id == item?.product?._id)?.length > 0 )
-                        //         return
-                        //     updateWishList(item, true, data?.data?.user?.wishList, dispatch, true, data?.data?.user?.cart, data?.data?.user?.videoCallCart);
-                        // });
-
-                        // guestVideoCallCart.forEach((item : ICartItem) => {
-                        //     if ( guestVideoCallCart?.filter((cartItem : ICartItem) => cartItem?.product?._id == item?.product?._id)?.length > 0 )
-                        //         return;
-                        //     updateVideoCallCart(item, true, false, data?.data?.user?.cart, dispatch, true, data?.data?.user?.videoCallCart, data?.data?.user?.wishList);
-                        // });
-
-                        // const currentUserResponse = await fetch(`${import.meta.env.VITE_BACKEND_URL}${import.meta.env.VITE_PORT}${import.meta.env.VITE_API_URL}users/current-user`, {
-                        //     method: "GET",
-                        //     headers: {
-                        //         "Content-Type": "application/json",
-                        //     },
-                            // credentials: "include"
-                        // });
-                        // console.log(currentUserResponse);
-              
-                        // if (!currentUserResponse.ok) {
-                        //   if ( currentUserResponse.statusText == "Unauthorized" || currentUserResponse.status == 401 ) {
-                            // console.log(await createGuestUser());
-                        //   }
-                        //   throw new Error("HTTP error! status: "+currentUserResponse.status+", "+currentUserResponse.statusText);
-                        // }
-                        // const currentUserData = await currentUserResponse.json();
-              
-                        // console.log("currentUserData?.data: ", currentUserData?.data, "data?.data: ", data?.data);
-              
-                        // if (data.data.role !== "Customer") throw new Error(`Error: ${401}, Unauthorised user`);
-                        // dispatch(setCustomerData(currentUserData.data));
-              
                         dispatch(setCustomerData({ ...data?.data?.user, cart: updateCartData?.data?.cart }));
                         toast.success("Logged In successfully!", { icon: <ToastSuccess />, className: "!inria-serif-regular !border-[#A68A7E] !text-[#A68A7E] !bg-white" });
                         navigate("/");
@@ -302,41 +170,6 @@ export const AuthenticationComponent = ({ type } : { type: string }) => {
                         console.log(error);
                     }
                 }}/>
-                    {/* <Form {...loginForm}>
-                        <form className="w-full flex flex-col gap-4 justify-center items-center" onSubmit={loginForm.handleSubmit(onLoginFormSubmit)}>
-                            <FormField
-                                control={loginForm.control}
-                                name="email"
-                                render={({ field }) => {
-                                    return <FormItem className="flex w-full justify-center gap-4 items-center flex-col">
-                                        <FormControl className="">
-                                            <ThemeProvider theme={textFieldTheme}>
-                                                <TextField {...field} fullWidth size="small" label="Email" variant="outlined" className="" />
-                                            </ThemeProvider>
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>;
-                                }}
-                            />
-                            <FormField
-                                name="password"
-                                control={loginForm.control}
-                                render={({ field }) => {
-                                    return <FormItem className="flex w-full justify-center gap-4 items-center flex-col">
-                                        <FormControl className="p-0 m-0">
-                                            <ThemeProvider theme={textFieldTheme}>
-                                                <TextField {...field} InputProps={{
-                                                    endAdornment: <IconButton onClick={handleClickShowPassword}>{showPassword ? <Eye className="w-4 h-4"/> : <EyeOff  className="w-4 h-4"/>}</IconButton>
-                                                }} type={showPassword ? "text" : "password"}  fullWidth size="small" label="Password" variant="outlined" className="" />
-                                            </ThemeProvider>
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>    
-                                }}
-                            />
-                            <Button disabled={isLoginButtonLoading} className="mt-4 w-16 bg-yellow-100 text-yellow-300 hover:text-yellow-500 hover:bg-yellow-300" variant={"ghost"} type="submit">{isLoginButtonLoading ? <Loader2 className="animate-spin" /> : "Login"}</Button> 
-                        </form>
-                    </Form> */}
                 </div>
             </div>
         </div>
